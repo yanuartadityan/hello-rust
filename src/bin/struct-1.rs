b9c122a27698b1f4ee9711e6a2da5947be302bcd
Yanuar T. Aditya Nugraha <yanuar.aditya@smarteye.se>
2023-03-12 12:57:59 +0100

    Updated Cargo

--- src/bin/struct-1.rs
+++ src/bin/struct-1.rs
@@ -1,3 +1,6 @@
+use std::io::{self, Read};
+
+#[derive(Debug)]
 struct User {
     active: bool,
     username: String,
@@ -12,4 +15,18 @@ fn main() {
         email: String::from("mirkwoodape@gmail.com"),
         sign_in_count: 1
     };
+
+    let b = User {
+        active: false,
+        username: {
+            let mut asu: String = String::new();
+            io::stdin()
+                .read_line(&mut asu)
+                .expect("Failed to parse username..");
+            asu.trim().to_string()
+        },
+        ..a
+    };
+
+    println!("b: {:?}", b);
 }
