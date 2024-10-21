5dc26fb8e108221b79e12ed955789faa89867d95
Yanuar T. Aditya Nugraha <yanuar.aditya@smarteye.se>
2024-10-21 02:27:49 +0200

    Add enum.rs

--- src/bin/enum.rs
+++ src/bin/enum.rs
@@ -3,17 +3,21 @@ use std::fmt;
 #[derive(Debug)]
 enum IpAddrKind {
     V4(u8, u8, u8, u8),
-    V6(String)
+    V6(String),
 }
 
 struct IpAddr {
     kind: IpAddrKind,
-    address: String
+    address: String,
 }
 
 impl fmt::Display for IpAddr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\n{{ \n  ip address: {:?} \n  address: {} \n}}", self.kind, self.address)
+        write!(
+            f,
+            "\n{{ \n  ip address: {:?} \n  address: {} \n}}",
+            self.kind, self.address
+        )
     }
 }
 
@@ -22,7 +26,7 @@ fn main() {
 
     let net_tv = IpAddr {
         kind: ipv4,
-        address: String::from("192.168.0.1")
+        address: String::from("192.168.0.1"),
     };
 
     println!("tv ip address: {}", net_tv);
